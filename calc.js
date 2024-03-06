13f5c8fe1d2a9e6813aeb3398a6ce7c230b8de3d
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2024-03-06 10:17:47 +0530

    calc

---
+++ calc.js
@@ -0,0 +1,26 @@
+var addition=function(a,b){
+    return a+b;
+
+}
+
+var subtraction=function(a,b){
+    return a-b;
+    
+}
+
+var multiplication=function(a,b){
+    return a*b;
+    
+}
+
+var division=function(a,b){
+    return a/b;
+    
+}
+
+module.exports={
+    addition:addition,
+    subtraction:subtraction,
+    multiplication:multiplication,
+    division:division
+}
